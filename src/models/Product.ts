8af5c9f79d90e9541e98186141b42d847704fc13
Victor Diaz <victor@legalkarma.io>
2023-06-18 12:52:29 -0600

    Add Product model

---
+++ src/models/Product.ts
@@ -0,0 +1,26 @@
+export enum NutriScore {
+  A = 'A',
+  B = 'B',
+  C = 'C',
+  D = 'D',
+  E = 'E',
+}
+
+export type Product = {
+  name: string
+  description: string
+  nutriScore: NutriScore
+  ingredients: string
+  details: string
+  weight: number
+  calories: number
+  fat: number
+  carbs: number
+  ean: number
+  ecologicalFootprint: number
+  co2: number
+  carbonFootprint: number
+  image: {
+    url: string
+  }
+}
